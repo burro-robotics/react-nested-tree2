aea6235550b008adefe38b458c227eaa2b1609b9
Paul Jones <paul@burro.ai>
2024-05-22 17:08:53 -0400

    Correct export types

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { TreeBranch } from "./TreeBranch";
+import { TreeData } from "./TreeData";
+import { TreeLeaf } from "./TreeLeaf";
+import { TreeRoot } from "./TreeRoot";
+
+export { TreeBranch, TreeLeaf, TreeRoot };
+
+export type { TreeData };
