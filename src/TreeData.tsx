cdc8bfaba0a7997404bcd30cfdbc8f0be06a0481
Paul Jones <paul@burro.ai>
2024-05-22 17:00:48 -0400

    formatting pass

--- src/TreeData.tsx
+++ src/TreeData.tsx
@@ -1,5 +1,5 @@
 export type TreeData<BranchData, LeafData> = {
-  id: string | number
-  data?: BranchData | LeafData
-  children?: TreeData<BranchData, LeafData>[]
-}
+  id: string | number;
+  data?: BranchData | LeafData;
+  children?: TreeData<BranchData, LeafData>[];
+};
--- src/TreeLeaf.tsx
+++ src/TreeLeaf.tsx
@@ -1,41 +1,11 @@
 import React from "react";
-import { TreeRoot } from "react-nested-tree";
+import type { TreeData } from "./TreeData";
 
-const App = () => {
-  return (
-    <TreeRoot<{ name: string }, { name: string }>
-      style={{ marginLeft: "20px" }}
-      data={{
-        id: "root",
-        children: [
-          {
-            id: 1,
-            data: { name: "* Branch 1" },
-            children: [
-              { id: 2, data: { name: "- Leaf 1-1" } },
-              { id: 3, data: { name: "- Leaf 1-2" } },
-              {
-                id: 4,
-                data: { name: "* Branch 1-4" },
-                children: [{ id: 5, data: { name: "- Leaf 1-4-1" } }],
-              },
-            ],
-          },
-          {
-            id: 2,
-            data: { name: "* Branch 2" },
-            children: [{ id: 5, data: { name: "- Leaf 2-1" } }],
-          },
-        ],
-      }}
-      renderLeaf={(leaf) => {
-        return <p>{leaf.name}</p>;
-      }}
-      renderBranch={(branch, _isToggled, onToggle) => {
-        return <button onClick={onToggle}>{branch.name}</button>;
-      }}
-    />
-  );
+export const TreeLeaf: React.FC<
+  {
+    item: TreeData<any, any>;
+    renderLeaf: (item: any) => React.ReactNode;
+  } & React.HTMLAttributes<HTMLLIElement>
+> = ({ item, renderLeaf, ...props }) => {
+  return <li {...props}>{renderLeaf(item.data)}</li>;
 };
-
-export default App;
