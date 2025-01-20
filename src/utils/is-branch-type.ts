2ab98486c0b94bef86f74f6c3bb104026e3c3936
Paul Jones <paul@burro.ai>
2025-01-20 15:07:03 -0500

    Allow branches to have empty children

--- src/utils/is-branch-type.ts
+++ src/utils/is-branch-type.ts
@@ -3,5 +3,5 @@ import {TreeData} from '../tree-data';
 export function isBranchType<BranchType, LeafType>(
   item: TreeData<BranchType, LeafType>,
 ): item is TreeData<BranchType, LeafType> & {data: BranchType} {
-  return !!item.children && item.children.length > 0;
+  return !!item.children;
 }
